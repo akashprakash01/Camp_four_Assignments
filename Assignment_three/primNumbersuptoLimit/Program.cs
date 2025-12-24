5218d169b019fe2dcb34bc5f96d66780798cfa61
akashprakash01 <akash700638@gmail.com>
2025-12-24 13:22:17 +0530

    Add prime numbers assignment and gitignore

---
+++ Assignment_three/primNumbersuptoLimit/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PrimeNumbersUpToLimit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // getting the limit from the user
+            Console.Write("Enter the limit: ");
+            int limit = int.Parse(Console.ReadLine());
+
+            //for loop for outer loop
+            for (int i = 2; i <= limit; i++)
+            {
+                //count initialization
+                int count = 0;
+
+                //inner loop
+                for (int j = 1; j <= i; j++)
+                {
+                    // if rem is zero then count will be incremented
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+
+                //checks whether count exactly 2 if yes it is a prime number
+                if (count == 2)
+                {
+                    Console.WriteLine($"{i} is a prime number");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
