b2872ed8f01fe2c4ced9b86d6d6fb29671b5e1be
akashprakash01 <akash700638@gmail.com>
2025-12-29 23:05:13 +0530

    Class and object assignemnt second Question done.(Employee salary)

---
+++ Assignment_Class_and_Object/EmployeeSalary/Model/Employee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary.Model
+{
+    public class Employee
+    {
+        //these fields store the data of the emoloyee
+        private string name;
+        private double baseSalary;
+        private int yearsOfService;
+
+        //constructor
+        public Employee()
+        {
+
+        }
+
+        //parameterized constructor
+        public Employee(String _name, double _baaseSalary, int _yearsOfService)
+        {
+            this.name = _name;
+            this.baseSalary = _baaseSalary;
+            this.yearsOfService = _yearsOfService;
+        }
+
+        //Encapsulation
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                //name validation
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Name cannot be empty");
+                else
+                    name = value;
+            }
+        }
+
+        public double BaseSalary
+        {
+            get { return baseSalary; }
+            set
+            {
+                if (value < 10000)
+                    Console.WriteLine("Base Salary cannot be less than 10000");
+                else
+                    baseSalary = value;
+            }
+        }
+        public int YearsOfService
+        {
+            get { return yearsOfService; }
+            set { yearsOfService = value; }
+        }
+        public double CalculateBonus()
+        {
+            return baseSalary * 0.05 * yearsOfService;
+        }
+
+        public double TotalSalary()
+        {
+            return baseSalary + CalculateBonus();
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Base Salary: {baseSalary}, Years of Service: {yearsOfService}, " +
+                   $"Bonus: {CalculateBonus()}, Total Salary: {TotalSalary()}";
+        }
+    }
+
+}
---
+++ Assignment_Class_and_Object/EmployeeSalary/Program.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeSalary.Model;
+
+namespace EmployeeSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display application heading
+            Console.WriteLine("------Employee Details------");
+
+            // Create an Employee object using the default constructor
+            Employee employeeOne = new Employee();
+
+            // Assign employee details using properties
+            employeeOne.Name = "Sam";
+            employeeOne.BaseSalary = 20000;
+            employeeOne.YearsOfService = 7;
+
+            // Display employee salary details using overridden ToString()
+            Console.WriteLine(employeeOne.ToString());
+
+            // Pause the console to view output
+            Console.ReadKey();
+        }
+    }
+}
