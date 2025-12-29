9f0ba16a978061ba2c66b099b1baae2b3e9bab38
akashprakash01 <akash700638@gmail.com>
2025-12-29 22:22:28 +0530

    Class and object assignemnt fisrt Question done.(Product inventory)

---
+++ Assignment_Class_and_Object/ProductInventory/Model/Product.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventory.Model
+{
+    public class Product
+    {
+        // these fields sotre the actual data of the product
+        private int productId;
+        private string name;
+        private double price;
+        private int stockQuantity;
+
+
+        //constructor
+        public Product()
+        {
+            
+        }
+        //parameterized constructor
+        public Product(int _productId, string _name, double _price, int _stockQuantity)
+        {
+            this.productId = _productId;
+            this.name = _name;
+            this.price = _price;
+            this.stockQuantity = _stockQuantity;
+        }
+
+        //Encapsulation
+        public int ProductId 
+            {
+            get
+            { return productId; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Product Id should be positive");
+                else
+                    productId = value;
+            }
+            }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Product Name should not be empty");
+                else
+                    name = value;
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Price must be greater than 0");
+                else
+                    price = value;
+            }
+        }
+
+        public int StockQuantity
+        {
+            get { return stockQuantity; }
+            set { stockQuantity = value; }
+        }
+
+        public bool AddStock()
+        {
+            stockQuantity++;
+            Console.WriteLine($"Stock updated for the product {name}");
+            return true;
+        }
+
+        public bool Sell()
+        {
+            if (stockQuantity <= 0)
+            {
+                Console.WriteLine("Product Out of Stock");
+                return false;
+            }
+
+            else 
+            {
+                stockQuantity--;
+                Console.WriteLine($"One stock has been sold for the product {name}");
+                return true;
+            }      
+        }
+
+        public override string? ToString()
+        {
+            return $"ProductId: {ProductId}, Product Name: {name}, Quantity: {stockQuantity} Price: {price}";
+        }
+    }
+}
---
+++ Assignment_Class_and_Object/ProductInventory/Program.cs
@@ -0,0 +1,32 @@
+﻿using ProductInventory.Model;
+
+namespace ProductInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Product Inventory----------");
+            // Create a Product object and initialize its properties
+            Product IPhone = new Product();
+            IPhone.ProductId = 1;
+            IPhone.Name = "IPhone16";
+            IPhone.StockQuantity = 0;
+            IPhone.Price = 80000;
+
+            // Display  product details
+            Console.WriteLine(IPhone.ToString());
+
+            // calling adding stock function and then Displaying product details
+            IPhone.AddStock();
+            Console.WriteLine(IPhone.ToString());
+
+            // calling sell  function  and then Displaying product details
+            IPhone.Sell();
+            Console.WriteLine(IPhone.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
