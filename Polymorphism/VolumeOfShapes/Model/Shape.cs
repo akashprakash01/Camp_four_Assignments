7a0ddc4c619b4e50b57438edc7d461a6c866c244
akashprakash01 <akash700638@gmail.com>
2026-01-02 20:42:38 +0530

    Q4 completed volume of shapes

---
+++ Polymorphism/VolumeOfShapes/Model/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeOfShapes.Model
+{
+    public class Shape
+    {
+        //default constructor
+        public Shape() { }
+        // Default volume Cube with side 10
+        public void Volume()
+        {
+            int side = 10;
+            Console.WriteLine("Volume of Cube (default): " + (side * side * side));
+        }
+
+        // Cube
+        public void Volume(int side)
+        {
+            Console.WriteLine("Volume of Cube: " + (side * side * side));
+        }
+
+        // Sphere
+        public void Volume(double radius)
+        {
+            double volume = (4.0 / 3.0) * Math.PI * radius * radius * radius;
+            Console.WriteLine("Volume of Sphere: " + volume);
+        }
+
+        // Cylinder
+        public void Volume(double radius, double height)
+        {
+            double volume = Math.PI * radius * radius * height;
+            Console.WriteLine("Volume of Cylinder: " + volume);
+        }
+    }
+}
---
+++ Polymorphism/VolumeOfShapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using VolumeOfShapes.Model;
+using System;
+
+namespace VolumeOfShapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Program to find the volume of Cube, Sphere, Cylinder ----");
+
+            Shape shape = new Shape();
+            // Default cube
+            shape.Volume();
+            // Cube
+            shape.Volume(12);
+            // Sphere
+            shape.Volume(5.5);
+            // Cylinder
+            shape.Volume(12, 2);        
+
+            Console.ReadKey();
+        }
+    }
+}
